e058667e0bbabcbbfc9b890cdd4711ddb8bb44ac
yasmin-ferreira26 <yasminsoaresferreira0.9@gmail.com>
2021-06-26 01:14:25 +0000

    pwii-navegacao

--- pwii-navegacao/src/App.js
+++ pwii-navegacao/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+
+import ContadorCompleto from './components/ContadorCompleto';
+import Card from './components/layout/Card';
+
+export default() =>(
+  <div className="App">
+    <h1>Contador</h1>
+  
+   <div className="Cards">
+      <Card titulo= "contador em um componente único" color="#02044f">
+        <ContadorCompleto numeroInicial={10}/>
+     </Card>
     </div>
-  );
-}
-
-export default App;
+  </div>
+)
+ 
